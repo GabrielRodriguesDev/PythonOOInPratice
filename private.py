d9a82c9cb5212e52a71cf88ae1627efed7eed80a
Gabriel Rodrigues <gabriel.rodrigues.mota@outlook.com>
2021-08-24 23:21:35 -0300

    Definindo métodos e váriaveis privadas e Abstraindo valores nas classes

---
+++ private.py
@@ -0,0 +1,13 @@
+class Test:
+    __variavel_static = 'variavel static'
+    
+    
+    def __init__(self):
+        self.__variavel = 'variavel'
+
+    def __method(self):
+        print('method private')
+        
+    @staticmethod
+    def __method__static():
+        print('method static private')
