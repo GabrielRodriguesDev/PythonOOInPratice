c8351b9355d5c7a45c1b2b8dbe4ad34ee69699c4
Gabriel Rodrigues <gabriel.rodrigues.mota@outlook.com>
2021-08-29 01:17:45 -0300

    Montando classe de escrita das cédulas do caixa eletrônico

--- file.py
+++ file.py
@@ -1,15 +1,22 @@
 import os
-
-class MoneySlipsFileReader:
+class BankFile:
     BASE_PATH =  'D:/Technology/Sistemas/Gabriel/Python/PythonOOInPratice/Files'
     
     def __init__(self):
-        self.__file = None
+        self._file = None
+    
+    def _openFileBank(self, mode):
+        return open(BankFile.BASE_PATH + '/_bank_file.dat', mode)
+    
+class MoneySlipsFileReader(BankFile):
+    
+    def __init__(self):
+        super().__init__() #Se referenciando ao construtor da classse pai
         self.__moneySlips = {}
     
     def getMoneySlips(self):
-        self.__file = self.__openFileBank('r')
-        line = self.__file.readline()
+        self._file = self._openFileBank('r')
+        line = self._file.readline()
         while self.__hasSemiColon(line):
             semiColonPosition = line.find(';')
             moneyBillValue = line[0:semiColonPosition]
@@ -27,9 +34,6 @@ class MoneySlipsFileReader:
     def __hasSemiColon(self,line):
         return line.find(';') != -1 
     
-    def __openFileBank(self, mode):
-        return open(MoneySlipsFileReader.BASE_PATH + '/_bank_file.dat', mode)
-    
     def __addMoneySlipsFromFileLine(self, moneyBillValue):
         equalPosition = moneyBillValue.find('=') #2
         moneyBill = moneyBillValue[0:equalPosition] #20  
@@ -40,3 +44,25 @@ class MoneySlipsFileReader:
 
 
 
+class MoneySlipsFileWriter(BankFile):
+    def writeMoneySlips(self, moneySplips):
+        lines = self.__readlines()
+        lines[0] = self.__formatLinesToWrite(moneySplips)
+        self.writeLines(lines)
+    
+    def __readlines(self):
+        self._file = self._openFileBank('w')
+        lines = self._file.readlines()
+        self._file.close()
+        return lines
+    
+    def writeLines(self, lines):
+        self._file = self._openFileBank('w')
+        self._file.writelines(lines)
+        self._file.close()
+    
+    def __formatLinesToWrite(self, moneySplips):
+        line = ""
+        for moneyBill, value in moneySplips.items():
+            line += self._file.write(moneyBill + '=' + str(value) + ';')
+        return line + '\n'
