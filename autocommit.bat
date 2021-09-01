06961ed39b9db67d6b818b1c77ba82597789dd6c
Gabriel Rodrigues <gabriel.rodrigues.mota@outlook.com>
2021-09-01 01:04:07 -0300

    Update 01/09/2021 -  1:04:07 - Final

--- autocommit.bat
+++ autocommit.bat
@@ -1,2 +1,3 @@
-git commit -am "Update %date:~0,2%/%date:~3,2%/%date:~-4% - %time:~0,2%:%time:~3,2%:%time:~6,2% - Testando escrita de contas bancárias e finalizando o projeto "
+git add .
+git commit -am "Update %date:~0,2%/%date:~3,2%/%date:~-4% - %time:~0,2%:%time:~3,2%:%time:~6,2% - Final"
 git push origin student
